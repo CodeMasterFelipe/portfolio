c8442f38aa819e42a71c0cf340ba6c099b8b45d6
Felipe Rivera <felipe.rivera.9803@gmail.com>
2024-03-31 23:11:04 -0700

    Partially real project data added
    This is a very rough draft of the projects and only containing
    the latest projects so far.

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -3,20 +3,23 @@ import { ProjectTile } from "../components/ProjectTile"
 
 const projects = [
   {
-    title: "Project One",
-    description: "This is a short description of my first project. It was built using React, Node.js, and MongoDB.",
+    title: "LLM Chat Agents",
+    description: "As my interest in ML and smart system have existed always inside me, I dive deeper on making a useful chatbot. \
+      It has different character that you can customize and talk to. I also created the option to generate images which can be created \
+      for the profile of the different characters. The bots have the ability of also searching the web.",
     link: "https://example.com/project-one",
     code: "https://github.com/yourusername/project-one"
   },
   {
-    title: "Project Two",
-    description: "An overview of my second project, highlighting the use of TypeScript, GraphQL, and Apollo Client.",
+    title: "MacOS Tiling Window Manager",
+    description: "MacOS is missing good window managment so I decided to create a hybrid tiling window managment that could benefit my 32:9 monitor.",
     link: "https://example.com/project-two",
     code: "https://github.com/yourusername/project-two"
   },
   {
-    title: "Project Three",
-    description: "An overview of my second project, highlighting the use of TypeScript, GraphQL, and Apollo Client.",
+    title: "React Minimalist Portfolio",
+    description: "I finally decided to spend the time to build a website to showcase what I am capable of. \
+      I love simplicity and minimalism and thus I make it guide me in the design of my portfolio.",
     link: "https://example.com/project-two",
     code: "https://github.com/yourusername/project-two"
   },
