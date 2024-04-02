ec233e685901d4bc01af09abf00ac96ee672e0c1
Felipe Rivera <felipe.rivera.9803@gmail.com>
2024-04-01 22:14:26 -0700

    project data links added and rm when not needed

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -8,21 +8,21 @@ const projects = [
     description: "As my interest in ML and smart system have existed always inside me, I dive deeper on making a useful chatbot. \
       It has different character that you can customize and talk to. I also created the option to generate images which can be created \
       for the profile of the different characters. The bots have the ability of also searching the web.",
-    link: "https://example.com/project-one",
-    code: "https://github.com/yourusername/project-one"
+    link: "",
+    code: ""
   },
   {
     title: "MacOS Tiling Window Manager",
     description: "MacOS is missing good window managment so I decided to create a hybrid tiling window managment that could benefit my 32:9 monitor.",
-    link: "https://example.com/project-two",
-    code: "https://github.com/yourusername/project-two"
+    link: "",
+    code: "https://github.com/CodeMasterFelipe/WinMan"
   },
   {
     title: "React Minimalist Portfolio",
     description: "I finally decided to spend the time to build a website to showcase what I am capable of. \
       I love simplicity and minimalism and thus I make it guide me in the design of my portfolio.",
-    link: "https://example.com/project-two",
-    code: "https://github.com/yourusername/project-two"
+    link: "",
+    code: "https://github.com/CodeMasterFelipe/portfolio"
   },
   // Add more projects as needed
 ];
